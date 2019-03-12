eb217064f46b54b0c5539680cb81a5cf669ed1b5
PETER SHI <532289386@qq.com>
2019-03-12 14:56:45 +0800

    Update ViewController.swift

--- SwiftDevUtilityDemo/ViewController.swift
+++ SwiftDevUtilityDemo/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        print("Hello world")
         testStringExtension()
         testArrayExtension()
         testDictionaryExtension()
